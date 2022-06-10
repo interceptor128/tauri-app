a4c3ce33985b4f5636cc60aeea7481fbfc6d26bf
interceptor128 <polestar.interceptor.0128@gmail.com>
2022-06-10 22:32:29 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import ToDoInputForm from "./ToDoInputForm.svelte";
+	import ToDoList from "./ToDoList.svelte";
+	let nameEntered = false
+	let lastName = ""
+	let firstName = ""
+
+	$: fullName = lastName + " " + firstName
+
+	function handleSubmit(){
+		if (lastName && firstName) {
+			nameEntered = true
+		}
+	}
+</script>
+
+<main>
+{#if nameEntered}
+	<h1>タスクリストアプリケーションへようこそ、{fullName}さん!</h1>
+	<ToDoInputForm userName={fullName} />
+	<ToDoList />
+{:else}
+	<h1>タスクリストアプリケーションへようこそ!</h1>
+	<h3>名前を入力してください。</h3>
+	<form on:submit|preventDefault={handleSubmit}>
+		<div>
+			<input bind:value={lastName} type="text" placeholder="姓" required>
+		</div>
+		<div>
+			<input bind:value={firstName} type="text" placeholder="名" required>
+		</div>
+		<button type="submit">タスク管理を始める</button>
+	</form>
+{/if}
+
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 450px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: none;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
